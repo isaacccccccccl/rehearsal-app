cea81c57733a373ed63f93fd5eaf6c13ba1756c7
Isaac Levy <isaaclevy004@gmail.com>
2025-05-20 14:12:50 +0300

    Update CORS configuration for production

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ import { songRoutes } from './api/songs/song.routes.js'
 import { setupSocketAPI } from './services/socket.service.js'
 import { logger } from './services/logger.service.js'
 
-
 const app = express()
 const server = http.createServer(app)
 
@@ -19,18 +18,21 @@ const server = http.createServer(app)
 app.use(cookieParser())
 app.use(express.json())
 
+// CORS configuration
+const corsOptions = {
+    origin: [   
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
+        'https://rehearsal-app.onrender.com'
+    ],
+    credentials: true
+}
+app.use(cors(corsOptions))
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve('public')))
-} else {
-    const corsOptions = {
-        origin: [   'http://127.0.0.1:3000',
-                    'http://localhost:3000',
-                    'http://127.0.0.1:5173',
-                    'http://localhost:5173'
-                ],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
 }
 
 app.use('/api/auth', authRoutes)
